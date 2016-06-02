f1e8d61de0603500779ea97906bab1f451df4934
zhufengpeixun1 <894918097@qq.com>
2016-06-02 16:16:23 +0800

    "package.json"

--- 14.angular/server.js
+++ 14.angular/server.js
@@ -13,13 +13,20 @@ http.createServer(function (req,res) {
     }else if(pathname=='/favicon.ico'){
         res.end('404');
     }else if(pathname=='/ajax'){
+        res.setHeader('Content-Type','application/json;charset=utf8');
+        //console.log(urlObj.query);
         var arr = [
-            {name:'javascript',count:1,price:30},
+            {name:'扎瓦岛',count:1,price:30},
             {name:'nodejs',count:1,price:50},
             {name:'Angularjs',count:1,price:80},
         ];
+        console.log(urlObj.query);
+        req.on('data', function (data) {
+            console.log(11,data.toString());
+        });
         res.end(JSON.stringify(arr));
     }else{
+        //bootstrap.css.map
         res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
         fs.createReadStream('.'+pathname).pipe(res);
     }
